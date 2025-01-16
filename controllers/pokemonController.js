7a2a21710c372623d0c630b57ed4dfafca1d1066
vivek <vivekdodiya1510@gmail.com>
2025-01-16 15:24:37 +0530

    commit

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -8,10 +8,15 @@ const test = (req, res) => {
 const getAllPokemon = async (req, res) => {
     try {
         const { page = 1, limit = 12 } = req.query;
-        const skip = (page - 1) * limit;
+        const skip = Number(page - 1) * Number(limit);
         const total = await Pokemon.find().countDocuments();
-        const pokemons = await Pokemon.find().skip(skip).limit(limit);
-        res.status(200).json({ pokemons, total });
+        const pokemons = await Pokemon.find().skip(skip).limit(Number(limit));
+        if (total > page * limit) {
+            nextLink = `http://${req.hostname}:${process.env.PORT || 4000
+                }/pokemon/pokemons/?page=${Number(page) + 1
+                }&limit=${limit}`;
+            res.status(200).json({ pokemons, total, nextLink });
+        }
     }
     catch (error) {
         res.status(404).json({ message: "Error fetching Pokemon" });
@@ -37,12 +42,12 @@ const getPokemonByname = async (req, res) => {
 const getPokemonByType = async (req, res) => {
     try {
         const type = req.params.type;
-        const pokemons = await Pokemon.find({ type }, {_id:0,id:1,'name.english':1, type : 1});
+        const pokemons = await Pokemon.find({ type }, { _id: 0, id: 1, 'name.english': 1, type: 1 });
         if (pokemons.length === 0) {
             return res.status(404).json({ message: "Pokemon not found" })
         }
         const total = await Pokemon.find().countDocuments();
-        res.status(200).json({ pokemons, total , length : pokemons.length});
+        res.status(200).json({ pokemons, total, length: pokemons.length });
     }
     catch (error) {
         res.status(404).json({ message: "Error fetching Pokemon", err: error.message });
